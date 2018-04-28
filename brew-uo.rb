09d07be8a4b4d8e612f7722356739e9639af7c0d
hexium310 <hexium310@gmail.com>
2018-04-28 15:35:57 +0900

    Update version

--- brew-uo.rb
+++ brew-uo.rb
@@ -1,11 +1,11 @@
 class BrewUo < Formula
   REPOSITORY_URL = "https://github.com/hexium310/brew-uo".freeze
-  VERSION = "0.0.1".freeze
+  VERSION = "0.0.2".freeze
 
   desc("Continuously execute 'brew update' and 'brew outdated'")
   homepage(REPOSITORY_URL)
   url("#{REPOSITORY_URL}/archive/#{VERSION}.tar.gz")
-  sha256("61114bc41499a53ea151e67c9442096e7b73323c8fba7329308f5558a46e9d92")
+  sha256("bb540636f1b73811297a96c107400c565a9bb1a3760599a8e12cb4919f9e4a92")
   head("#{REPOSITORY_URL}.git", :tag => VERSION)
 
   def install
