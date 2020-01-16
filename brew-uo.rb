cf9939eebf42629f744251e2b22d9cd00a2885ef
Hexin <hexium310@gmail.com>
2020-01-17 02:48:48 +0900

    Refactor

--- brew-uo.rb
+++ brew-uo.rb
@@ -1,18 +1,18 @@
 class BrewUo < Formula
-  REPOSITORY_URL = "https://github.com/hexium310/brew-uo".freeze
-  VERSION = "0.2.1".freeze
+  REPOSITORY_URL = "https://github.com/hexium310/brew-uo"
+  VERSION = "0.2.1"
 
-  desc("Continuously execute 'brew update' and 'brew outdated'")
-  homepage(REPOSITORY_URL)
-  url("#{REPOSITORY_URL}/releases/download/#{VERSION}/#{VERSION}.tar.gz")
-  sha256("5611a29c99a344e0de6a86296b9ec1e5754367a8ce8c48b11f9bac1a63492402")
-  head("#{REPOSITORY_URL}.git", :tag => VERSION)
+  desc "Continuously execute 'brew update' and 'brew outdated'"
+  homepage REPOSITORY_URL
+  url "#{REPOSITORY_URL}/releases/download/#{VERSION}/#{VERSION}.tar.gz"
+  sha256 "5611a29c99a344e0de6a86296b9ec1e5754367a8ce8c48b11f9bac1a63492402"
+  head "#{REPOSITORY_URL}.git", :tag => VERSION
 
   def install
-    bin.install("brew-uo")
+    bin.install "brew-uo"
   end
 
   test do
-    system("false")
+    system "false"
   end
 end
