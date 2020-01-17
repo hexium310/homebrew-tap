2d7e85b98f43f07a8155e6b15225f8a6488a4991
Hexin <hexium310@gmail.com>
2020-01-17 19:56:40 +0900

    Fix download url

--- brew-uo.rb
+++ brew-uo.rb
@@ -4,7 +4,7 @@ class BrewUo < Formula
 
   desc "Continuously execute 'brew update' and 'brew outdated'"
   homepage REPOSITORY_URL
-  url "#{REPOSITORY_URL}/releases/download/#{VERSION}/#{VERSION}.tar.gz"
+  url "#{REPOSITORY_URL}/releases/download/#{VERSION}/macos.tar.gz"
   sha256 "39cbbcf609317fe0d1ffe68bbd73d4d0290765b8d461bd303a16f75e99f702d9"
   head "#{REPOSITORY_URL}.git", :tag => VERSION
 
