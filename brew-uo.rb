17c3f6adc3331a47d76dfe3ca84b6fb5cb37d9cc
Hexin <hexium310@gmail.com>
2020-01-18 18:22:17 +0000

    Update version to 0.3.1

--- brew-uo.rb
+++ brew-uo.rb
@@ -1,11 +1,11 @@
 class BrewUo < Formula
   REPOSITORY_URL = "https://github.com/hexium310/brew-uo"
-  VERSION = "0.3.0"
+  VERSION = "0.3.1"
 
   desc "Continuously execute 'brew update' and 'brew outdated'"
   homepage REPOSITORY_URL
   url "#{REPOSITORY_URL}/releases/download/#{VERSION}/macos.tar.gz"
-  sha256 "39cbbcf609317fe0d1ffe68bbd73d4d0290765b8d461bd303a16f75e99f702d9"
+  sha256 "061828aaebe81020f2b2f0b415d1b21554c0f10770c0639281766d2d014dbad8"
   head "#{REPOSITORY_URL}.git", :tag => VERSION
 
   def install
