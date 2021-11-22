0aaf418095144c5bb1a961e20ef96de3d02a921a
Hexin <hexium310@gmail.com>
2021-11-23 02:24:57 +0900

    Stop using variables to use `brew bump-formula-pr`

--- Formula/brew-uo.rb
+++ Formula/brew-uo.rb
@@ -1,12 +1,9 @@
 class BrewUo < Formula
-  REPOSITORY_URL = "https://github.com/hexium310/brew-uo"
-  VERSION = "0.3.2"
-
   desc "Continuously execute 'brew update' and 'brew outdated'"
-  homepage REPOSITORY_URL
-  url "#{REPOSITORY_URL}/releases/download/#{VERSION}/macos.tar.gz"
+  homepage "https://github.com/hexium310/brew-uo"
+  url "https://github.com/hexium310/brew-uo/releases/download/0.3.2/macos.tar.gz"
   sha256 "2bdb2f9375979fc6f8eb1d3619a3b6fad00e9c7469231e841d3cdd206aec6e36"
-  head "#{REPOSITORY_URL}.git", :tag => VERSION
+  head "https://github.com/hexium310/brew-uo.git", :tag => "0.3.2"
 
   def install
     bin.install "brew-uo"
