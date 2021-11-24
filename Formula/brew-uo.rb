fa1cb6bd3950eab143af0c2d1e1374a8d38f0a56
Hexin <hexium310@gmail.com>
2021-11-25 03:18:39 +0900

    Remove head in brwe-uo

--- Formula/brew-uo.rb
+++ Formula/brew-uo.rb
@@ -3,7 +3,6 @@ class BrewUo < Formula
   homepage "https://github.com/hexium310/brew-uo"
   url "https://github.com/hexium310/brew-uo/releases/download/0.4.0/macos.tar.gz"
   sha256 "726427be10d35b43c636f852910c05187a96a66885eff86afdf579fd18655246"
-  head "https://github.com/hexium310/brew-uo.git", tag: "0.3.2"
 
   def install
     bin.install "brew-uo"
