998de8dda70e64042aa70364d019d929ed7fb8fa
Hexin <hexium310@gmail.com>
2021-11-25 03:10:14 +0900

    Use new Ruby 1.9 hash syntax

--- Formula/brew-uo.rb
+++ Formula/brew-uo.rb
@@ -3,7 +3,7 @@ class BrewUo < Formula
   homepage "https://github.com/hexium310/brew-uo"
   url "https://github.com/hexium310/brew-uo/releases/download/0.3.2/macos.tar.gz"
   sha256 "2bdb2f9375979fc6f8eb1d3619a3b6fad00e9c7469231e841d3cdd206aec6e36"
-  head "https://github.com/hexium310/brew-uo.git", :tag => "0.3.2"
+  head "https://github.com/hexium310/brew-uo.git", tag: "0.3.2"
 
   def install
     bin.install "brew-uo"
