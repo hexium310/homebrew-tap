74cd8351a80d86fe59c1ef87d70982fb5f35c7df
runner <runner@Mac-1687978067121.local>
2023-06-28 18:53:46 +0000

    brew-uo 0.4.8

--- Formula/brew-uo.rb
+++ Formula/brew-uo.rb
@@ -1,8 +1,8 @@
 class BrewUo < Formula
   desc "Continuously execute 'brew update' and 'brew outdated'"
   homepage "https://github.com/hexium310/brew-uo"
-  url "https://github.com/hexium310/brew-uo/releases/download/0.4.7/macos.tar.gz"
-  sha256 "c0628032b63d88e88a0150c55cf0efbbeff0c609b0bae1c622c7983b381245b5"
+  url "https://github.com/hexium310/brew-uo/releases/download/0.4.8/macos.tar.gz"
+  sha256 "b5e6deb09b70bcc2a16c20bad1f973179d9fa40de29eae149c1c80f8205f3abc"
 
   def install
     bin.install "brew-uo"
